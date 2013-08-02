9ede3af4f2a1e27176e2980209fd4aeed8ee006a
David Cottrell <davidrcottrell@gmail.com>
2013-08-01 21:08:32 -0400

    Add dampening and velocity tracking

--- TapeMeasure/MeasureHelper.m
+++ TapeMeasure/MeasureHelper.m
@@ -16,6 +16,20 @@
 @property (nonatomic, strong) NSOperationQueue *queue;
 
 @property (nonatomic, assign) NSTimeInterval previousTimeStamp;
+
+@property (nonatomic, assign) double px;
+@property (nonatomic, assign) double py;
+@property (nonatomic, assign) double pz;
+
+@property (nonatomic, assign) double vx;
+@property (nonatomic, assign) double vy;
+@property (nonatomic, assign) double vz;
+
+
+@property (nonatomic, assign) NSTimeInterval timeSinceMeaningfulAccelerationX;
+@property (nonatomic, assign) NSTimeInterval timeSinceMeaningfulAccelerationY;
+@property (nonatomic, assign) NSTimeInterval timeSinceMeaningfulAccelerationZ;
+
 @end
 
 @implementation MeasureHelper
@@ -28,27 +42,85 @@
         _motionManager = [[CMMotionManager alloc] init];
         _queue = [[NSOperationQueue alloc] init];
         _queue.maxConcurrentOperationCount = 1;
-        _previousTimeStamp = -1.0;
     }
     return self;
 }
 
-- (double)distanceWithAccelerationX:(double)x accelerationY:(double)y accelerationZ:(double)z time:(NSTimeInterval)deltaT
+- (void)initializeValues
 {
-#pragma message("TODO: implement")
-    return 0.0;
+    _previousTimeStamp = -1.0;
+    
+    _px = _py = _pz = 0.0;
+    _vx = _vy = _vz = 0.0;
+}
+
+
+- (double)distanceWithX:(double)x y:(double)y z:(double)z
+{
+    return sqrt(x*x + y*y + z*z);
 }
 
 - (void)computeUpdatedDistance:(CMDeviceMotion *)motion
 {
-    DLog(@"motion: %@", motion);
     if (_previousTimeStamp != -1.0) {
         
-        double addedDistance = [self distanceWithAccelerationX:motion.userAcceleration.x accelerationY:motion.userAcceleration.y accelerationZ:motion.userAcceleration.z time:(motion.timestamp-_previousTimeStamp)];
+        NSTimeInterval deltaT = motion.timestamp-_previousTimeStamp;
         
-#pragma message("TODO: add to current tally")
+        double ax = motion.userAcceleration.x;
+        double ay = motion.userAcceleration.y;
+        double az = motion.userAcceleration.z;
         
+        if (fabs(ax) < 0.01) {
+            _timeSinceMeaningfulAccelerationX += deltaT;
+        } else {
+            _timeSinceMeaningfulAccelerationX = 0.0;
+        }
         
+        if (fabs(ay) < 0.01) {
+            _timeSinceMeaningfulAccelerationY += deltaT;
+        } else {
+            _timeSinceMeaningfulAccelerationY = 0.0;
+        }
+        if (fabs(az) < 0.04) {
+            _timeSinceMeaningfulAccelerationZ += deltaT;
+        } else {
+            _timeSinceMeaningfulAccelerationZ = 0.0;
+        }
+        
+        if (_timeSinceMeaningfulAccelerationX > 0.1) {
+            _vx *= 0.75;
+        } else {
+            _vx += ax * deltaT;
+        }
+        if (_timeSinceMeaningfulAccelerationY > 0.1) {
+            _vy *= 0.75;
+        } else {
+            _vy += ax * deltaT;
+        }
+        
+        if (_timeSinceMeaningfulAccelerationZ > 0.1) {
+            _vz *= 0.5;
+            DLog(@"dampen");
+        } else {
+            _vz += ax * deltaT;
+        }
+        
+        _px += _vx * deltaT;
+        _py += _vy * deltaT;
+        _pz += _vz * deltaT;
+
+//        DLog(@"v:%f", _vz);
+//        DLog(@"accel:%f", motion.userAcceleration.z);
+//        DLog(@"x:%f", _px*980.6);
+//        DLog(@"y:%f", _py*980.6);
+//        DLog(@"z:%f", _pz*980.6);
+
+        
+//        DLog(@"y:%f", _py);
+//        DLog(@"z:%f", _pz);
+
+        
+        DLog(@"Total Distance: %f", [self distanceWithX:_px y:_py z:_pz] * 980.6);
     }
     
     _previousTimeStamp = motion.timestamp;
@@ -57,6 +129,9 @@
 
 - (void)startMeasure
 {
+    [self initializeValues];
+    _motionManager.deviceMotionUpdateInterval = 0.01;
+    
     [_motionManager startDeviceMotionUpdatesToQueue:_queue withHandler:^(CMDeviceMotion *motion, NSError *error) {
         if (error) {
             DLog(@"ERROR: %@", error);
